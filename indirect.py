6be115f136971ff355b090c35e9fa966d8f109a6
Huy Tran <huy.tranquoc@asnet.com.vn>
2016-02-18 10:52:07 +0700

    Update log info

--- indirect.py
+++ indirect.py
@@ -1,9 +1,15 @@
 from Naked.toolshed.shell import execute_js
 
 # success case
-success = execute_js('scripts/parent.js')
+success = execute_js('scripts/parent.js --code=0')
+print '==============================================='
+print 'Run script with exit code 0'
 print 'Sample script is done with result: %s' % success
+print '==============================================='
 
 # failed case
 failed = execute_js('scripts/parent.js --code=1')
+print '==============================================='
+print 'Run script with exit code 1'
 print 'Sample script is done with result: %s' % failed
+print '==============================================='
